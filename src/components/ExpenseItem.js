aab22f2c1ff153831753a686f8b8f8b7a3a7c7a9
HarshitNegi-A <harshitnegi226@gmail.com>
2024-10-03 16:21:46 +0530

    Styling Components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,15 @@
 // Write your code here:
+import "./ExpenseItem.css"
 function ExpenseItem(){
     return(
-      <>
-      <div>Date</div>
-      <div>Delhi</div>
-      <div>
-        <h2>Title</h2>
-        <div>Amount</div>
+      <div className="expense-item">
+      <div>15 August 2023</div>
+      <div className="expense-item__location">Delhi</div>
+      <div className="expense-item__description">
+        <h2>Book</h2>
+        <div className="expense-item__price">$10</div>
       </div>
-      </>
+      </div>
     );
   }
   
